f298e6edef06d9381b6b6651839ea9697d8bc2b8
Cstan0824 <149685655+Cstan0824@users.noreply.github.com>
2025-04-30 19:26:09 +0800

    Update functions.php

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -11,13 +11,26 @@ add_action( 'wp_enqueue_scripts', function () {
 function rewrite_asset_urls_to_s3($url) {
     $cdn_base = 'https://tarumt-grad-hub-bucket.s3.us-east-1.amazonaws.com';
 
-    if (strpos($url, '/wp-includes/') !== false || strpos($url, '/wp-content/') !== false) {
-        $parsed = parse_url($url);
-        if (isset($parsed['path'])) {
-            $url = $cdn_base . $parsed['path'];
-        }
+    // Prevent rewriting if it's already an S3 URL
+    if (strpos($url, $cdn_base) !== false) {
+        return $url;
     }
 
+    // Get the current site's base URL (e.g., http://elb.amazonaws.com/tarumtgradhub)
+    $site_url = site_url();
+
+    // If $url starts with the site_url, strip it and keep only the path
+    if (strpos($url, $site_url) === 0) {
+        $path = substr($url, strlen($site_url));
+
+        // Clean leading slash just in case
+        $path = '/' . ltrim($path, '/');
+
+        // Return full S3 path
+        return rtrim($cdn_base, '/') . $path;
+    }
+
+    // Otherwise, return original
     return $url;
 }
 
