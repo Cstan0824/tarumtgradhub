a9b80501cdba9005e57c6d8786b9a51a88d1ff11
Cstan0824 <149685655+Cstan0824@users.noreply.github.com>
2025-04-30 19:31:46 +0800

    Update functions.php

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -9,7 +9,7 @@ add_action( 'wp_enqueue_scripts', function () {
 });
 
 function rewrite_asset_urls_to_s3($url) {
-    $cdn_base = 'https://tarumt-grad-hub-bucket.s3.us-east-1.amazonaws.com';
+    $cdn_base = 'https://tarumt-grad-hub-bucket.s3.us-east-1.amazonaws.com/ccb_assignment';
 
     // Prevent rewriting if it's already an S3 URL
     if (strpos($url, $cdn_base) !== false) {
